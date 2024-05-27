73560f5e3b91ef81315055d5b781c3bdcd7df68c
akotonystark <alarbiampofo@gmail.com>
2024-05-27 19:11:19 -0400

    recursion

---
+++ sortingAlgorithms.js
@@ -0,0 +1,21 @@
+function swap(a,b){
+    let temp = a
+    a = b
+    b = temp
+    return [a,b]
+}
+function bubbleSort(list){
+    for(let i=0; i<list.length; i++){
+        for(let j=0; j<list.length - i; j++){
+            if(list[j] > list[j+1]){
+                //swap(list[i],list[i+1])
+                let temp = list[j]
+                list[j] = list[j+1]
+                list[j+1] = temp
+            }
+        }
+    }
+    console.log(list)
+} //Worst case is O(n)squared best case is O(n)
+
+bubbleSort([3,5,3,7,9,1,8,8,0,5,22,5,7,89,11])
