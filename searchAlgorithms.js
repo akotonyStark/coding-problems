4a8c6b19d315748131f2609fd2164f567c22961a
akotonystark <alarbiampofo@gmail.com>
2024-05-28 15:17:40 -0400

    binary search bug fix

--- searchAlgorithms.js
+++ searchAlgorithms.js
@@ -12,25 +12,21 @@ function binarySearch(list, target){
     console.log('running binary search...')
     let left = 0
     let right = list.length - 1
-
+    let mid
     while(left <= right){
-        let mid = Math.floor(list.length/2)
+        mid = left + Math.floor((right - left) / 2);
         if(list[mid] == target){
-            console.log("Found ", target)
-            break
+            return mid
         }
         else if(target < list[mid]){
             right = mid - 1 
-            console.log("Found ", target)
-            break
         }
         else{
             left = mid + 1
-            console.log("Found ", target)
-            break
         }
     }
+    return -1
 }
 
 linearSearch([3,5,3,7,9,1,8], 9)
-binarySearch([1,2,4,6,8,22,45,47,52,55,89,90,98,99], 22)
+console.log(binarySearch([1,2,4,6,8,22,45,47,52,55,89,90,98,99], 22))
