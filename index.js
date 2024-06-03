6125e4ac6dabc92888ee59c2906bbd80466597dc
akotonystark <alarbiampofo@gmail.com>
2024-06-03 01:21:41 -0400

    productExceptSelf

--- index.js
+++ index.js
@@ -346,7 +346,39 @@ function reverseWords(s){
     console.log(arrOfWords.reverse().join(' '))
 }
 
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+*/
+
+function productExceptSelf(arr){
+    let copy = [...arr]
+    let answer = []
+    for(let i=0; i<arr.length; i++){
+        copy.splice(i,1)
+        answer.push(copy)
+        copy = [...arr]
+    }
+
+    console.log(answer.map((item) => item.reduce((acc, x) => acc * x)))
+}
+
+
+productExceptSelf([1,2,3,4])
 
 
 
