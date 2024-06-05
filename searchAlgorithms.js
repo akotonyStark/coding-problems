89a44c7e10e65d03c082ef9fa8fedd6842221670
akotonystark <alarbiampofo@gmail.com>
2024-06-05 13:26:55 -0400

    recursive binary search

--- searchAlgorithms.js
+++ searchAlgorithms.js
@@ -2,7 +2,7 @@ function linearSearch(list , target){
     console.log('running linear search...')
     for(let i=0; i<list.length; i++){
         if(list[i] == target){
-            console.log('Found')
+            console.log('Found ', target, ' at position ', i)
             break;
         }
     }
@@ -28,5 +28,26 @@ function binarySearch(list, target){
     return -1
 }
 
+
+function recursiveBinarySearch(list, target){
+    console.log('running recursive binary search...', list)
+    if(list.length == 0){
+        return false
+    }
+    else{
+        let mid =   Math.floor(list.length / 2);
+        if(list[mid] == target){
+            return mid
+        }
+        else if(target > list[mid]){
+            return recursiveBinarySearch(list.slice(mid+1), target)
+        }
+        else{
+            return recursiveBinarySearch(list.slice(0, mid), target)
+        }
+    }
+}
+
 linearSearch([3,5,3,7,9,1,8], 9)
 console.log(binarySearch([1,2,4,6,8,22,45,47,52,55,89,90,98,99], 22))
+console.log(recursiveBinarySearch([1,2,4,6,8,22,45,47,52,55,89,90,98,99], 52))
